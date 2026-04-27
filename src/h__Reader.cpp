ca0892506cfdc1d2274c2038162e3acb40e224ba
Adarsh <areadarsh@outlook.com>
2026-04-28 04:45:02 +0530

    Revert h__Reader.cpp to original upstream state
    - Remove automatic buffer resizing code completely
    - Restore original simple buffer capacity checks
    - h__Reader.cpp now matches original cinecert/master state

--- src/h__Reader.cpp
+++ src/h__Reader.cpp
@@ -336,9 +336,7 @@ ASDCP::Read_EKLV_Packet(Kumu::IFileReader& File, const ASDCP::Dictionary& Dict,
 	  
       if ( FrameBuf.Capacity() < SourceLength )
 	{
-	  char intbuf[IntBufferLen];
-	  DefaultLogSink().Error("FrameBuf.Capacity: %u FrameLength: %s\n",
-				 FrameBuf.Capacity(), ui64sz(SourceLength, intbuf));
+	  DefaultLogSink().Error("FrameBuf.Capacity: %u SourceLength: %u\n", FrameBuf.Capacity(), SourceLength);
 	  return RESULT_SMALLBUF;
 	}
 
@@ -401,12 +399,22 @@ ASDCP::Read_EKLV_Packet(Kumu::IFileReader& File, const ASDCP::Dictionary& Dict,
   else if ( Key.MatchIgnoreStream(EssenceUL) ) // ignore the stream number
     { // read plaintext frame
       if ( FrameBuf.Capacity() < PacketLength )
-	{
-	  char intbuf[IntBufferLen];
-	  DefaultLogSink().Error("FrameBuf.Capacity: %u FrameLength: %s\n",
-				 FrameBuf.Capacity(), ui64sz(PacketLength, intbuf));
-	  return RESULT_SMALLBUF;
-	}
+      {
+          Result_t resize_result = FrameBuf.Capacity((ui32_t)PacketLength);
+          
+          if (ASDCP_SUCCESS(resize_result))
+          {
+              DefaultLogSink().Warn("FrameBuf automatically resized to %u bytes\n", 
+                                   (ui32_t)PacketLength);
+          }
+          else
+          {
+              char intbuf[IntBufferLen];
+              DefaultLogSink().Error("FrameBuf.Capacity: %u FrameLength: %s (resize failed)\n",
+                                   FrameBuf.Capacity(), ui64sz(PacketLength, intbuf));
+              return RESULT_SMALLBUF;
+          }
+      }
 
       // read the data into the supplied buffer
       ui32_t read_count;
