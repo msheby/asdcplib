51804a7eb2bed040b2d8f714a4a91a2702e91e42
Adarsh <areadarsh@outlook.com>
2026-04-29 08:25:06 +0530

    updated to i64 for getLargestElement, explicit offset

--- src/MXF.cpp
+++ src/MXF.cpp
@@ -1302,46 +1302,51 @@ ASDCP::MXF::OPAtomIndexFooter::ContainerDuration() const
 }
 
 //
-ui32_t
+ui64_t
 ASDCP::MXF::OPAtomIndexFooter::GetLargestElementSize() const
 {
+  ui64_t last_offset = 0, largest_size = 0;
+  bool have_prev = false;
   std::list<InterchangeObject*>::iterator li;
 
   for ( li = m_PacketList->m_List.begin(); li != m_PacketList->m_List.end(); li++ )
     {
       IndexTableSegment *segment = dynamic_cast<IndexTableSegment*>(*li);
 
-      if ( segment != 0 && segment->EditUnitByteCount > 0 )
+      if ( segment == 0 )
+        continue;
+
+      if ( segment->EditUnitByteCount > 0 )
         return segment->EditUnitByteCount;
-    }
 
-  ui64_t last_offset = 0, largest_size = 0;
-  bool have_prev = false;
-
-  for ( li = m_PacketList->m_List.begin(); li != m_PacketList->m_List.end(); li++ )
-    {
-      IndexTableSegment *segment = dynamic_cast<IndexTableSegment*>(*li);
-
-      if ( segment != 0 )
+      for ( ui32_t i = 0; i < segment->IndexEntryArray.size(); ++i )
         {
-          for ( ui32_t i = 0; i < segment->IndexEntryArray.size(); ++i )
+          ui64_t offset = segment->IndexEntryArray[i].StreamOffset;
+
+          if ( have_prev )
             {
-              ui64_t offset = segment->IndexEntryArray[i].StreamOffset;
-
-              if ( have_prev )
-                {
-                  ui64_t this_size = offset - last_offset;
-                  if ( this_size > largest_size )
-                    largest_size = this_size;
-                }
-
-              last_offset = offset;
-              have_prev = true;
+              ui64_t this_size = offset - last_offset;
+              if ( this_size > largest_size )
+                largest_size = this_size;
             }
+
+          last_offset = offset;
+          have_prev = true;
         }
     }
 
-  return (ui32_t)largest_size;
+  if ( have_prev && m_ECOffset > 0 && (ui64_t)m_ECOffset < ThisPartition )
+    {
+      ui64_t essence_extent = ThisPartition - m_ECOffset;
+      if ( essence_extent > last_offset )
+        {
+          ui64_t last_element_size = essence_extent - last_offset;
+          if ( last_element_size > largest_size )
+            largest_size = last_element_size;
+        }
+    }
+
+  return largest_size;
 }
 
 //
